9411c494603dc34a399b19d317610e1391cc6f78
eduardotecnologo <edudeveloper@Eduardo-MacBook-Pro.local>
2021-08-24 19:16:19 -0300

    Campos de textos e Descrição

--- animal_book_app/lib/views/widgets/CustomInput.dart
+++ animal_book_app/lib/views/widgets/CustomInput.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class  CustomInput extends StatelessWidget {
   final TextEditingController controller;
@@ -6,6 +7,9 @@ class  CustomInput extends StatelessWidget {
   final bool obscure;
   final bool autofocus;
   final TextInputType type;
+  final int maxLines;
+  final List<TextInputFormatter> inputFormatters;
+  final Function(String) validator;
 
   // Construtor
   CustomInput({
@@ -13,18 +17,23 @@ class  CustomInput extends StatelessWidget {
     @required this.hint,
     this.obscure = false,
     this.autofocus = false,
-    this.type = TextInputType.text
+    this.type = TextInputType.text,
+    this.inputFormatters,
+    this.maxLines,
+    this.validator
   });
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
       controller: this.controller,
       obscureText: this.obscure,
       autofocus: this.autofocus,
       keyboardType: this.type,
+      inputFormatters: this.inputFormatters,
       style: TextStyle(fontSize: 20),
-
+      validator: this.validator,
+      maxLines: this.maxLines,
       decoration: InputDecoration(
           contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
           hintText: this.hint,
